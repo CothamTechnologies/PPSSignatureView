ee786157007fa87ce9927cb9d2050489cec22452
Soren Mortensen <soren@cothamtechnologies.com>
2019-06-24 14:28:08 +0100

    Update with Cotham Technologies additions

--- Source/PPSSignatureView.h
+++ Source/PPSSignatureView.h
@@ -6,7 +6,17 @@
 @property (assign, nonatomic) UIColor *strokeColor;
 @property (assign, nonatomic) BOOL hasSignature;
 @property (strong, nonatomic) UIImage *signatureImage;
+// CT Addition
+@property id signatureViewDelegate;
 
 - (void)erase;
+// CT Addition
+- (void)remoteSetSignatureImage:(UIImage *)signatureImage;
 
+@end
+
+// CT Addition
+@protocol CTPPSSignatureViewProtocol <NSObject>
+@optional
+- (void)signatureView:(PPSSignatureView *)signatureView didFinishEditingWithImage:(UIImage* )signatureImage;
 @end
